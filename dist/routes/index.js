e72e5989f291adf4dc08ec8a0723f60b10d9bc3d
GI LIM HONG <gilim.h5ng@gmail.com>
2019-05-03 16:20:11 +0900

    [CREATE] nodemon 설정 추가

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,9 +1,12 @@
 "use strict";
 
-var express = require('express');
+var _express = _interopRequireDefault(require("express"));
 
-var router = express.Router();
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var router = _express["default"].Router();
 /* GET home page. */
+
 
 router.get('/', function (req, res, next) {
   res.render('index', {
